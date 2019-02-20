108052443654b823b921c5c96aec78cbfa15809c
Misha Dmitruk <dmitruk.m@inbox.ru>
2019-02-20 13:27:08 +0300

    1

--- Instructions/Startup.cs
+++ Instructions/Startup.cs
@@ -46,7 +46,7 @@ namespace Instructions
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
-       //
+      //
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
