688093527b9b8d86aa06744413393dfcf972e47a
Misha Dmitruk <dmitruk.m@inbox.ru>
2019-02-21 00:07:24 +0300

    some

---
+++ Instructions/Models/User.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Instructions.Models
+{
+    public class User : IdentityUser
+    {
+        public string Language { get; set; }
+        public bool Color { get; set; }
+        public bool RoleISAdmin { get; set; }
+        public bool Status { get; set; }
+    }
+}
